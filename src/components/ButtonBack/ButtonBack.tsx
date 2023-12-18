db398951168a4353a7697ae20715ba9bc9df7958
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-12-18 09:02:36 +0100

    feat: add City component

---
+++ src/components/ButtonBack/ButtonBack.tsx
@@ -0,0 +1,9 @@
+const ButtonBack: React.FC = () => {
+  return (
+    <div>
+      <button>Back Button</button>
+    </div>
+  );
+};
+
+export default ButtonBack;
---
+++ src/helpers/formatDate.ts
@@ -0,0 +1,8 @@
+export const formatDate = (date: string): string | null => {
+  return new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    weekday: 'long',
+  }).format(new Date(date));
+};
