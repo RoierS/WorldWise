66bfbd423bd50e4f6dd978774055a0f09d1becaf
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-12-21 10:25:04 +0100

    feat: implement map displaying

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,33 +1,35 @@
+import { useState } from 'react';
+
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import styles from './Map.module.css';
 
+const MAP_TOKEN = import.meta.env.VITE_MAP_TOKEN;
+
 const Map: React.FC = () => {
   const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState<[number, number]>([40, 0]);
+
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
+  // const lat = searchParams.get('lat');
+  // const lng = searchParams.get('lng');
 
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate('form');
-      }}
-    >
-      <h1>Position:</h1>
-      <p>
-        {lat}, {lng}
-      </p>
-
-      <button
-        onClick={() => {
-          setSearchParams({ lat: '23', lng: '50' });
-        }}
-      >
-        Chamge pos
-      </button>
+    <div className={styles.mapContainer}>
+      <MapContainer className={styles.map} center={mapPosition} zoom={6} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='<a href="https://tomtom.com" target="_blank">&copy;  1992 - 2023 TomTom.</a> '
+          url={`https://{s}.api.tomtom.com/map/1/tile/basic/main/{z}/{x}/{y}.png?key=${MAP_TOKEN}`}
+          subdomains="abcd"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
